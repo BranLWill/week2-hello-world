ede0518d8dcc216261c73dac0a1ee3ee7ddf7c00
Brandon Willett <bwillett@mail.valenciacollege.edu>
2016-01-20 20:30:07 -0500

    Hello in different languages

---
+++ Hello in different languages.py
@@ -0,0 +1,42 @@
+# Brandon Willett
+
+print('Hello, pick your language of choice.') # ask for their choice of language
+
+# I Displayed their options
+print('Press 1: Dutch')
+print('Press 2: Italian')
+print('Press 3: Swedish')
+
+# I used opt for the variable
+opt = input()
+
+# then matched the numbers up with the proper language
+if opt == '1':
+    print('Hallo Wereld')
+if opt == '2':
+    print('Ciao mondo')
+if opt == '3':
+    print('Hej världen')
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
